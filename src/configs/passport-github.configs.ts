e1a736940ff398e987201924b04d9ab3f3dcab2a
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-03-16 06:44:33 +0200

    implement GitHub authentication with Passport.js and add user model

---
+++ src/configs/passport-github.configs.ts
@@ -0,0 +1,29 @@
+import passport from "passport";
+import { Strategy as GitHubStrategy } from "passport-github";
+import dotenv from "dotenv";
+dotenv.config();
+
+const gitHubClientID = process.env.GITHUB_CLIENT_ID as string;
+const gitHubClientSecret = process.env.GITHUB_CLIENT_SECRET as string;
+
+passport.use(new GitHubStrategy({
+   clientID: gitHubClientID,
+   clientSecret: gitHubClientSecret,
+   callbackURL: "http://localhost:3000/api/v0/auth/github/callback",
+},
+async (accessToken, refreshToken, profile, done) => {
+   return done(null, profile);
+}));
+
+passport.serializeUser(function(user: any, cb) {
+   process.nextTick(function() {
+     cb(null, user);
+   });
+});
+
+passport.deserializeUser(function(user: any, cb) {
+   process.nextTick(function() {
+     return cb(null, user);
+   });
+});
+
---
+++ src/interfaces/auth.interfaces.ts
@@ -0,0 +1,26 @@
+import { Document } from "mongoose";
+import { Request } from "express";
+export interface Profile {
+   id: string;
+   emails?: { value: string }[];
+   username?: string;
+   displayName: string;
+   provider: string;
+   photos?: { value: string }[];
+   refreshToken?: string;
+}
+
+export interface IUser extends Document {
+  _id: string; // the id will become the result of random uuid coming from crypto package
+  password?:string;
+  email?: string;
+  userName?: string;
+  displayName: string;
+  profileImage?: string;
+  refreshToken:string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface IAuthRequest extends Request{
+  user: IUser;
+}
