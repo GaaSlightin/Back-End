a65aba05d7f7358041158abdad76db8c8774cfb0
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2025-03-17 04:36:34 +0200

    Revert "Feat/job archival"

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,21 @@
-import express from "express";
-import dotenv from "dotenv";
-import { dbConnection } from "./db/db";
-import passport from "passport";
-import session from "express-session";
-import cookieParser from "cookie-parser";
-import routes from "./routes";
-dotenv.config();
-const app = express();
-const port = process.env.PORT || 8080;
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const app=express()
+const port=process.env.PORT||8080
 
-(async () => {
-  await dbConnection();
-})();
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 
-app.use(passport.initialize());
-app.use(passport.session());
-// API routes
-app.use("/api/v0/", routes);
-
-app.use("/", (req, res) => {
-  res.json({
-    status: "Done",
-    message: "server running",
-  });
-});
-
-app.listen(port, (err) => {
-  if (err) {
-    console.log("Cannot initialize the server");
-  }
-  console.log(`Running server on port ${port}`);
-});
+app.use("/",(req,res)=>{
+   res.json({
+      status:"Done",
+      message:"server running"
+   })
+})
+app.listen(port,(err)=>{
+   if(err){
+      console.log("Cannot initalize the server")
+   }
+   console.log(`Running server on port ${port}`)
+})
