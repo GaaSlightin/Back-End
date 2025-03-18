1ed03b15e0aca36ed10a78dc2a0113e653e9a22e
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2025-03-18 20:47:03 +0200

    hotfix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@
 import { Router } from "express";
 import authRouter from "./auth.router";
 import githubRouter from "./github.router";
+import jobRouter from "./job.routes";
 
 
 const router = Router();
 
 router.use("/auth", authRouter)
 router.use("/github",githubRouter)
+router.use("/jobs", jobRouter);
 
 export default router;
