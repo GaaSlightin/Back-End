e974e3a2cb0dfbc13395f12efc05f4a637f91aa7
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-03-18 22:02:41 +0200

    feat(auth): update GitHub callback to use dynamic failure redirect URL

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -2,11 +2,13 @@ import "../configs/passport-github.configs";
 import express, { RequestHandler } from "express";
 import passport from "passport";
 import { handleGithubAuth } from "../controllers/auth/auth.controller";
+import dotenv from "dotenv"
+dotenv.config()
 
 const authRouter = express.Router();
 
 authRouter.get("/github", passport.authenticate("github"));
-authRouter.get("/github/callback", passport.authenticate('github', { failureRedirect: '/failure' }), handleGithubAuth);
+authRouter.get("/github/callback", passport.authenticate('github', { failureRedirect: `${process.env.FRONTEND_URL}/auth-failure` }), handleGithubAuth);
 authRouter.get("/failure", (req, res) => {
    res.status(401).json({
      status: "fail",
