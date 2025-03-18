7fad739574d8b00c3cd857646c1c4136f6ab4b8b
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-03-19 01:10:01 +0200

    fix(cors): update CORS configuration to allow requests from any origin

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+  origin:true,
   credentials: true
 }));
 
